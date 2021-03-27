38b65fdca9bd8edcf37246377cfa6c2d75b7e50a
BoboMac <30493004+BoboMac@users.noreply.github.com>
2021-03-27 13:31:58 +0200

    Add files via upload

--- generate_info.py
+++ generate_info.py
@@ -6,8 +6,18 @@ dir = "pos"
 for f in os.scandir(dir):
 	line = ""
 	filename = f.name
-	img = Image.open(dir + "\\" + filename)
-	line = ("pos/" + filename + " 0 0 " + str(img.size[0]) + " " + str(img.size[1]) + "\n")
+	img = Image.open(dir + "/" + filename)
+	line = ("pos/" + filename + " 1 0 0 " + str(img.size[0]) + " " + str(img.size[1]) + "\n")
 	outf.write(line)
 	print(line)
+outf.close()
 
+# negatives
+noutf = open("neg.txt", "w")
+dir = "neg"
+for f in os.scandir(dir):
+	line = ""
+	filename = f.name
+	line = ("neg/" + filename + "\n")
+	noutf.write(line)
+	print(line)
